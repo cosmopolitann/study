f279798870ac701828251907c61fda897f15fbf4
winter <woaimengke@126.com>
2021-07-14 15:03:23 +0800

    article 取消点赞 同步 pubsub

--- mvc/article_cancel_like.go
+++ mvc/article_cancel_like.go
@@ -1,6 +1,7 @@
 package mvc
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 
@@ -49,22 +50,22 @@ func ArticleCancelLike(ipfsNode *ipfsCore.IpfsNode, db *Sql, value string) (Arti
 	// query
 
 	// //publish msg
-	// topic := "/db-online-sync"
-	// sugar.Log.Info("发布主题:", "/db-online-sync")
-	// sugar.Log.Info("发布消息:", value)
+	topic := "/db-online-sync"
+	sugar.Log.Info("发布主题:", "/db-online-sync")
+	sugar.Log.Info("发布消息:", value)
 
-	// ctx := context.Background()
+	ctx := context.Background()
 
-	// tp, ok := TopicJoin.Load(topic)
-	// if !ok {
-	// 	tp, err = ipfsNode.PubSub.Join(topic)
-	// 	if err != nil {
-	// 		sugar.Log.Error("PubSub.Join .Err is", err)
-	// 		return err
-	// 	}
-	// 	TopicJoin.Store(topic, tp)
-	// }
-	rows, err := db.DB.Query("SELECT id,IFNULL(user_id,'null'),IFNULL(article_id,'null'),IFNULL(is_like,0) FROM article_like where article_id=?", art.Id)
+	tp, ok := TopicJoin.Load(topic)
+	if !ok {
+		tp, err = ipfsNode.PubSub.Join(topic)
+		if err != nil {
+			sugar.Log.Error("PubSub.Join .Err is", err)
+			return dl, err
+		}
+		TopicJoin.Store(topic, tp)
+	}
+	rows, err := db.DB.Query("SELECT id,IFNULL(user_id,'null'),IFNULL(article_id,'null'),IFNULL(is_like,0) FROM article_like where article_id=? and user_id=?", art.Id, userid)
 	if err != nil {
 		sugar.Log.Error("Query article_like is failed.Err is ", err)
 		return dl, err
@@ -78,40 +79,40 @@ func ArticleCancelLike(ipfsNode *ipfsCore.IpfsNode, db *Sql, value string) (Arti
 	}
 	defer rows.Close()
 
-	// sugar.Log.Info("--- 开始 发布的消息 ---")
+	sugar.Log.Info("--- 开始 发布的消息 ---")
 
-	// sugar.Log.Info("发布的消息:", value)
-	// //
-	// //第一步
-	// var s3 ArticleCanCelLike
-	// s3.Type = "receiveArticleCancelLike"
-	// s3.Data = dl
-	// s3.FromId = ipfsNode.Identity.String()
+	sugar.Log.Info("发布的消息:", value)
+	//
+	//第一步
+	var s3 ArticleCanCelLike
+	s3.Type = "receiveArticleCancelLike"
+	s3.Data = dl
+	s3.FromId = ipfsNode.Identity.String()
 
-	// jsonBytes, err := json.Marshal(s3)
-	// if err != nil {
-	// 	sugar.Log.Info("--- 开始 发布的消息 ---")
-	// 	return err
-	// }
-	// sugar.Log.Info("--- 解析后的数据 返回给 转接服务器 ---", string(jsonBytes))
+	jsonBytes, err := json.Marshal(s3)
+	if err != nil {
+		sugar.Log.Info("--- 开始 发布的消息 ---")
+		return dl, err
+	}
+	sugar.Log.Info("--- 解析后的数据 返回给 转接服务器 ---", string(jsonBytes))
 
-	// //============================
-	// err = tp.Publish(ctx, jsonBytes)
-	// if err != nil {
-	// 	sugar.Log.Error("发布错误:", err)
-	// 	return err
-	// }
-	// sugar.Log.Error("---  发布的消息  完成  ---")
+	//============================
+	err = tp.Publish(ctx, jsonBytes)
+	if err != nil {
+		sugar.Log.Error("发布错误:", err)
+		return dl, err
+	}
+	sugar.Log.Error("---  发布的消息  完成  ---")
 
-	// //==
-	// err = tp.Publish(ctx, jsonBytes)
-	// if err != nil {
-	// 	sugar.Log.Error("发布错误:", err)
-	// 	return err
-	// }
-	// sugar.Log.Error("---  发布的消息  完成  ---")
-	// //---
-	// sugar.Log.Info("~~~~~  ArticleCancelLike   Method     End ~~~~~~")
+	//==
+	err = tp.Publish(ctx, jsonBytes)
+	if err != nil {
+		sugar.Log.Error("发布错误:", err)
+		return dl, err
+	}
+	sugar.Log.Error("---  发布的消息  完成  ---")
+	//---
+	sugar.Log.Info("~~~~~  ArticleCancelLike   Method     End ~~~~~~")
 	return dl, nil
 }
 
